ce6a3afdf4e764f98bf5c3f07c287b4a9d760f17
Housz <housz@housz.cn>
2024-01-17 11:39:17 +0800

    Update Footer.js

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -26,7 +26,9 @@ export const Footer = (props) => {
        <div className="flex align-baseline justify-between flex-wrap">
          <p>
            © {BLOG.AUTHOR} {copyrightDate}
+          <i class="fa-brands fa-github"></i>
          </p>
+           
        </div>
      </div>
    </footer>
